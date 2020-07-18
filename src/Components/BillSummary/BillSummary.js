45ba6d5ec6c71a08c06d63ea052be4d311e04000
hhon <alexhnhon@gmail.com>
2020-07-18 03:03:09 -0700

    styling

--- src/Components/BillSummary/BillSummary.js
+++ src/Components/BillSummary/BillSummary.js
@@ -7,8 +7,8 @@ function BillSummary(props) {
             <Grid item xs={4}></Grid>
             <Grid item xs={4}>
                 <div className='bills-total-cost'>
-                    <p>{props.period} bill cost: $ {props.period === 'monthly' ? props.totalCosts : props.periodCosts} </p>
-                    <p>{props.period} saved: $ {props.period === 'monthly' ? props.saved : props.periodSaved}</p>
+                    <p>{props.period[0].toUpperCase()+props.period.substring(1)} bill cost: $ {props.period === 'monthly' ? props.totalCosts : props.periodCosts} </p>
+                    <p>{props.period[0].toUpperCase()+props.period.substring(1)} saved: $ {props.period === 'monthly' ? props.saved : props.periodSaved}</p>
                 </div>
             </Grid>
             <Grid item xs={4}></Grid>
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, TextField, Button, Box } from '@material-ui/core';
 
 function Form(props) {
     return (
@@ -7,15 +7,11 @@ function Form(props) {
             <Grid item xs={4}></Grid>
             <Grid item xs={4}>
                 <form className='add-bill-form-control' onSubmit={props.addBill}>
-                    <div>
-                        <input type='text' placeholder='Bill Title' onChange={(e) => props.setBillTitle(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={props.billTitle} ></input>
-                    </div>
-                    <div>
-                        <input type='text' placeholder='Enter Monthly Bill' onChange={(e) => props.setBillAmount(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={props.billAmount} ></input>
-                    </div>
-                    <div>
-                        <input type='submit'></input>
-                    </div>
+                    <TextField type='text' label='Bill Title' onChange={(e) => props.setBillTitle(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={props.billTitle} ></TextField>
+                    <TextField type='text' label='Enter Monthly Bill' onChange={(e) => props.setBillAmount(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={props.billAmount} ></TextField>
+                    <Box mt={1}>
+                        <Button type='submit' variant='outlined'>Submit</Button>
+                    </Box>
                 </form>
             </Grid>
             <Grid item xs={4}></Grid>
