c8f0970f07ee2763247ccd9c4928bc570d11e150
hhon <alexhnhon@gmail.com>
2020-07-25 19:54:50 -0700

    add contact

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -29,21 +29,24 @@ function Form() {
     );
   }
 
-  useEffect(()=> {
-    localStorage.setItem('billsList',  JSON.stringify(billsList));
+  useEffect(() => {
+    localStorage.setItem('billsList', JSON.stringify(billsList));
   }, [billsList])
 
-  useEffect(()=> {
-    localStorage.setItem('costs',  JSON.stringify(costs));
+  useEffect(() => {
+    localStorage.setItem('costs', JSON.stringify(costs));
   }, [costs])
 
   return (
     <>
       <Grid item xs={1} sm={4}></Grid>
       <Grid item xs={10} sm={4}>
+        <a href='https://github.com/hnhon' rel="noopener noreferrer" target="_blank" style={{ color: 'black', textDecoration: 'none', margin:'auto' }}>Project Repo</a> <br />
+        <a href='https://linkedin.com/in/hiunamhon' rel="noopener noreferrer" target="_blank" style={{ color: 'black', textDecoration: 'none' }} >Contact Me</a> <br />
+        <Box mb={1} />
         <form className='add-bill-form-control' onSubmit={(e) => { addBill(e); updateCosts(parseFloat(billAmount)) }}>
-          <TextField type='text' label='Bill Title' onChange={(e) => setBillTitle(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={billTitle} required /><br/>
-          <TextField type='number' label='Monthly Bill' onChange={(e) => setBillAmount(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={billAmount} required /><br/>
+          <TextField type='text' label='Bill Title' onChange={(e) => setBillTitle(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={billTitle} required /><br />
+          <TextField type='number' label='Monthly Bill' onChange={(e) => setBillAmount(e.target.value)} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }} value={billAmount} required /><br />
           <Box mt={1}>
             <Button type='submit' variant='outlined'>Submit</Button>
           </Box>
