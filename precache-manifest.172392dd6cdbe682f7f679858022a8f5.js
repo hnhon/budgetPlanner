20aba5359595b6446911f51ea305705bf462a19e
hhon <alexhnhon@gmail.com>
2020-11-21 03:49:30 -0800

    Updates

---
+++ precache-manifest.172392dd6cdbe682f7f679858022a8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb63bf6e8742e74a938f5a5d7e2c419d",
+    "url": "/budgetPlanner/index.html"
+  },
+  {
+    "revision": "11f8d038a5c1014eb371",
+    "url": "/budgetPlanner/static/css/main.65b301cf.chunk.css"
+  },
+  {
+    "revision": "b9b5d615d7ccb9f8ffde",
+    "url": "/budgetPlanner/static/js/2.33db91a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/budgetPlanner/static/js/2.33db91a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11f8d038a5c1014eb371",
+    "url": "/budgetPlanner/static/js/main.0e9a9c00.chunk.js"
+  },
+  {
+    "revision": "42d79653a1f0e4d68b98",
+    "url": "/budgetPlanner/static/js/runtime-main.7e884bec.js"
+  }
+]);
